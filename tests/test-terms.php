30c096b8b46e1ed4fea2397fb139546e222d89d0
Tony Dennis <tony@delaris.com>
2016-11-22 18:10:44 -0800

    Framing api-tests using known good

---
+++ tests/test-terms.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Connections
+ */
+
+/**
+ * Sample test case.
+ */
+class TermsTest extends WP_UnitTestCase {
+
+
+
+	public function setUp() {
+		parent::setUp();
+
+		global $wp_rest_server;
+		global $route;
+
+		$this->server = $wp_rest_server = new WP_REST_Server;
+		$this->route = "/cn-api/v1/category";
+
+		do_action( 'rest_api_init' );
+
+		$this->subscriber = $this->factory->user->create( array( 'role' => 'subscriber' ) );
+		$this->administrator = $this->factory->user->create( array( 'role' => 'administrator' ) );
+	}
+
+	/**
+	 * Routes present
+	 */
+	public function test_register_routes() {
+		$routes = $this->server->get_routes();
+
+		$this->assertArrayHasKey( '/cn-api/v1/category', $routes );
+		$this->assertArrayHasKey( '/cn-api/v1/category/(?P<id>[\d]+)', $routes );
+	}
+	/**
+	 * Test that the route protexts itself from an unauthorized user
+	 */
+	public function test_get_unauthorized() {
+		wp_set_current_user( 0 );
+		$request = new WP_REST_Request( 'GET', $this->route );
+
+		$response = $this->server->dispatch( $request );
+		$this->assertResponseStatus( 401, $response );
+	}
+
+	/**
+	 * Test that there is a response for an authorized user
+	 */
+	public function test_get_authorized() {
+		wp_set_current_user( $this->subscriber );
+		$request = new WP_REST_Request( 'GET', $this->route );
+		$response = $this->server->dispatch( $request );
+		$this->assertResponseStatus( 200, $response );
+		print_r($response);
+
+	}
+
+	/**
+	 * Test that the route exists in the API
+	 */
+	function test_routeExists() {
+
+	}
+
+	// Helpers
+
+	protected function assertResponseStatus( $status, $response ) {
+		$this->assertEquals( $status, $response->get_status() );
+	}
+
+	protected function assertResponseData( $data, $response ) {
+		$response_data = $response->get_data();
+		$tested_data = array();
+		foreach( $data as $key => $value ) {
+			if ( isset( $response_data[ $key ] ) ) {
+				$tested_data[ $key ] = $response_data[ $key ];
+			} else {
+				$tested_data[ $key ] = null;
+			}
+		}
+		$this->assertEquals( $data, $tested_data );
+	}
+
+}
